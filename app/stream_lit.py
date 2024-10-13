c5f9e0a97b37c16934fcd04351ca15d731977c48
π <pirayan20@gmail.com>
2024-10-13 19:35:02 +0700

    Streamlit Added

---
+++ app/stream_lit.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import openai
+import os
+from llama_index.llms.openai import OpenAI
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+
+st.set_page_config(
+    page_title="Moneythunder Chatbot",
+    page_icon="🦙",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+st.title("Moneythunder Chatbot 💬🦙")
+
+if "messages" not in st.session_state.keys():  # Initialize the chat messages history
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Ask me a question about MoneyThunder Product!",
+        }
+    ]
+
+
+@st.cache_resource(show_spinner=True)
+def load_data():
+    reader = SimpleDirectoryReader(input_dir="./scripts/documents")
+    docs = reader.load_data()
+    Settings.llm = OpenAI(
+        model="gpt-4o-mini",
+        temperature=0.2,
+        system_prompt="""You are an expert on
+        the product MoneyThunder and your
+        job is to answer questions.
+        Assume that all questions are related
+        to the MoneyThunder product. Keep
+        your answers precise and based on
+        facts  do not hallucinate features.""",
+    )
+    index = VectorStoreIndex.from_documents(docs)
+    return index
+
+
+index = load_data()
+
+if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
+    st.session_state.chat_engine = index.as_chat_engine(
+        chat_mode="condense_question", verbose=True, streaming=True
+    )
+
+if prompt := st.chat_input(
+    "Ask a question"
+):  # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages:  # Write message history to UI
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        response_stream = st.session_state.chat_engine.stream_chat(prompt)
+        st.write_stream(response_stream.response_gen)
+        message = {"role": "assistant", "content": response_stream.response}
+        # Add response to message history
+        st.session_state.messages.append(message)
