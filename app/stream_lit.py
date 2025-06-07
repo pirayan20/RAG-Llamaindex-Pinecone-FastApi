f821619c4fcdc2cf6f99079a937cfbd341923bbc
π <pirayan20@gmail.com>
2025-06-08 00:17:12 +0700

    YEA

--- app/stream_lit.py
+++ app/stream_lit.py
@@ -25,7 +25,7 @@ if "messages" not in st.session_state.keys():  # Initialize the chat messages hi
 
 @st.cache_resource(show_spinner=True)
 def load_data():
-    reader = SimpleDirectoryReader(input_dir="./scripts/documents")
+    reader = SimpleDirectoryReader(input_dir="./app/scripts/documents")
     docs = reader.load_data()
     Settings.llm = OpenAI(
         model="gpt-4o-mini",
